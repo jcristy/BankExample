2770bb863a9f5b58799f1efb9c7d88b05900e6fb
Alan Baines <abaines@vt.edu>
2013-05-02 13:43:10 -0400

    baine's first commit

---
+++ List.h
@@ -0,0 +1,92 @@
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+
+typedef double LinkedStorage;
+
+
+
+struct Node
+{
+	LinkedStorage data;
+	Node* next;
+};
+
+
+
+class List
+{
+public:
+	Node* m_head;
+	
+	Node* LastNode();
+
+public:
+	List();
+	
+	LinkedStorage getIndex(int index);
+	
+	bool addElement(LinkedStorage elem);
+	
+	
+	void print();
+	
+
+};
+
+List::List()
+{
+	cout << "List()" << endl;
+	
+	
+	m_head = NULL;
+	
+}
+
+
+Node* List::LastNode()
+{
+	Node* trace = m_head;
+	
+	while (trace && trace->next)
+		trace = trace->next;
+	
+	if ( trace && trace->data )
+		cout << trace->data << endl;
+	else
+		cout << "NULL" << endl;
+		
+	return trace;
+}
+
+
+bool List::addElement(LinkedStorage elem)
+{
+	cout << "addElement " << elem << endl;
+	
+	
+	Node* temp = new Node();
+	
+	temp->next = NULL;
+	temp->data = elem;
+	
+	Node* last = LastNode();
+	
+//	if ( last )
+//		last->next = temp;
+//	else
+	
+	m_head->next = temp;
+	
+	
+
+
+	return true;
+}
+
---
+++ Test.cpp
@@ -0,0 +1,31 @@
+
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "List.h"
+
+using namespace std;
+
+
+
+
+
+int main()
+{
+
+	cout << "Hello" << endl;
+	
+	List L;
+	
+	L.addElement(5.5);
+	
+	
+	L.LastNode();
+
+	return 0;
+}
+
+
+

561fb60ad430b5eab35e3fe6d032b3613f1859b3
jcristy <jcristy@vt.edu>
2013-05-02 11:58:01 -0400

    Added Person.h

---
+++ Person.h
@@ -0,0 +1,13 @@
+#include <string>
+
+class Person{
+	public:
+		string name;
+		string phone_number;
+	private:
+		string getName();
+		string getPhone();
+		void setName(string name);
+		void setPhone(string phone);
+		
+	}

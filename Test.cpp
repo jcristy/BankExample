017d33230326ed0bd632a3b29fcc9af64e9387aa
Alan Baines <abaines@vt.edu>
2013-05-02 14:29:42 -0400

    List size

--- Test.cpp
+++ Test.cpp
@@ -20,9 +20,23 @@ int main()
 	List L;
 	
 	L.addElement(5.5);
+	L.addElement(6.6);
+	L.addElement(7.7);
+	L.addElement(8.8);
 	
 	
-	L.LastNode();
+	for (int i = 0 ; i < 10 ; i++)
+	{
+		try
+		{
+			cout << i << " " ;
+			cout << L.getIndex(i) << endl;
+		}
+		catch (...)
+		{
+			cout << "error on " << i << endl;
+		}
+	}
 
 	return 0;
 }
